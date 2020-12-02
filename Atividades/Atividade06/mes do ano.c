315dbf50614267cfdde71b9490d83343e3f0d5d9
Flávio Felix <71350515+fflaviofelix@users.noreply.github.com>
2020-12-02 15:55:48 -0300

    Update mes do ano.c

--- Atividades/Atividade06/mes do ano.c
+++ Atividades/Atividade06/mes do ano.c
@@ -4,46 +4,49 @@
 #include <locale.h>
 int main(){
     int opcao;
-    printf("\nEntre número de 1 a 12 para imprimir o mês correspondente: ");
+    opcao=0;
+    for(opcao=0; opcao < 13; opcao++)
+    {printf("\nEntre número de 1 a 12 para imprimir o mês correspondente: ");
     scanf("%d", &opcao);
-switch(opcao){
-    case 1:
-            printf("1.Janeiro\n");
-            break;
-    case 2:
-            printf("2.Fevereiro\n");
-            break;
-    case 3:
-            printf("3.Marco\n");
-            break;
-    case 4:
-            printf("4.Abril\n");
-            break;
-    case 5:
-            printf("5.Maio\n");
-            break;
-    case 6:
-            printf("6.Junho\n");
-            break;
-    case 7:
-            printf("7.Julho\n");
-            break;
-    case 8:
-            printf("8.Agosto\n");
-            break;
-    case 9:
-            printf("9.Setembro\n");
-            break;
-    case 10:
-            printf("10.Outubro\n");
-            break;
-    case 11:
-            printf("11.Novembro\n");
-            break;
-    case 12:
-            printf("12.Dezembro\n");
-            break;
-printf("\n\n");
+        switch(opcao){
+                case 1:
+                        printf("1.Janeiro");
+                        break;
+                case 2:
+                        printf("2.Fevereiro");
+                        break;
+                case 3:
+                        printf("3.Marco");
+                        break;
+                case 4:
+                        printf("4.Abril");
+                        break;
+                case 5:
+                        printf("5.Maio");
+                        break;
+                case 6:
+                        printf("6.Junho");
+                        break;
+                case 7:
+                        printf("7.Julho");
+                        break;
+                case 8:
+                        printf("8.Agosto");
+                        break;
+                case 9:
+                        printf("9.Setembro");
+                        break;
+                case 10:
+                        printf("10.Outubro");
+                        break;
+                case 11:
+                        printf("11.Novembro");
+                        break;
+                case 12:
+                        printf("12.Dezembro");
+                        break;
+        }
+printf("\n");
 }
 return 0;
 }
