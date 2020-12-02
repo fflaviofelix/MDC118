26bbd0bc4158e7e3c9d319f0933f2e7601dfaa38
Flávio Felix <71350515+fflaviofelix@users.noreply.github.com>
2020-12-02 16:20:18 -0300

    Update matriz.c

--- Atividades/Atividade05/matriz.c
+++ Atividades/Atividade05/matriz.c
@@ -1,5 +1,5 @@
-//Escrever um programa em C que localize um funcionário em uma matriz multidimensional a partir da sua matricula e imprima o respectivo telefone. 
-//Considere a matriz igual a {{1111, 30001000}, {2222, 30002000}, {3333, 30003000}, {4444, 30004000}, {5555, 30005000}, {6666, 30006000}, {7777, 30007000}}.
+/*Escrever um programa em C que localize um funcionário em uma matriz multidimensional a partir da sua matricula e imprima o respectivo telefone. 
+Considere a matriz igual a {{1111, 30001000}, {2222, 30002000}, {3333, 30003000}, {4444, 30004000}, {5555, 30005000}, {6666, 30006000}, {7777, 30007000}}.*/
 
 #include <stdio.h>
 #include <locale.h>
